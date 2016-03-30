4fb62a6910620611fa771ed9c3f242a7406c9e04
vincent salomon <vincent.salomon@zoho.com>
2016-03-30 16:02:12 +0200

    1ères scripts dans les interfaces circuit et composant. classement des 2 interfaces edans paquage application et définition d'un package Kernel vierge

---
+++ Projet boole AOO/src/application/Composant.java
@@ -0,0 +1,17 @@
+package application;
+
+public interface Composant {
+
+		/** numéro d'indentifiaction du composant
+	   * @return numéro du composant
+	   */
+	  public void numero_composant();
+	  /** nb de sortie du composant
+	   * @return nb de sortie du composant
+	   */
+	  public void nb_sortie();
+	  /** nb d'entrées du composant
+	   * @return nb d'entrées du composant
+	   */
+	  public void nb_entree();
+}
