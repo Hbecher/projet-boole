0f7cabc4ca52b850fd6a30ccaaf111b3feac4838
ZZZZZZ <ZZZZZZ@DESKTOP-0VOHF53>
2016-05-23 17:04:32 +0200

    Add4b

---
+++ Projet boole AOO/src/composantcomposite/Add4b.java
@@ -0,0 +1,50 @@
+package composantcomposite;
+
+public class Add4b {
+
+boolean s1,s2,s3,s4;
+	
+public int Aff4bC2(boolean e1,boolean e2,boolean e3,boolean e4)
+{
+	int i1,i2,i3,i4,res;
+
+	if(e1==true) i1=-1; else i1=1;
+	if(e2==true) i2=4; else i2=0;
+	if(e3==true) i3=2; else i3=0;
+	if(e4==true) i4=1; else i4=0;
+	
+	res = (i4+i3+i2)*i1;
+	
+	return res;
+	
+}
+
+public void Gen4bC2(int i)
+{
+	if(i<0) s1=false; else s1=true;
+	if (i<0) i=9-i;
+	
+	if(i-4>=0)
+	{
+		s2=true;
+		i=i-4;
+	}
+	else s2=false;
+	
+	if(i-2>=0)
+	{
+		s3=true;
+		i=i-2;
+	}
+	else s3=false;
+	
+	if(i-1>=0)
+	{
+		s4=true;
+		i=i-1;
+	}
+	else s4=false;
+	
+}
+
+}
